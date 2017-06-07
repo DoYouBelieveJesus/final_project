b99596bd04f65dcf116588b10cf6946c76689c7f
DoYouBelieveJesus <gg4627942@gmail.com>
2017-06-07 18:15:14 +0800

    first commit

---
+++ resources/views/master.blade.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+    <title> sds </title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="{{'css/home.css'}}" type="text/css">
+
+    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+<body>
+@include('layout.navbar')
+<div class="container">
+    <div class="content">
+        <div class="title">Hunger Game</div>
+        <div class="gameLotto" >
+            Lotto
+        </div>
+        <div class="gameLucky">
+            Lucky draw
+        </div>
+        <div class="gamerules">
+
+        </div>
+
+
+
+
+    </div>
+</div>
+
+
+</body>
+</html>
