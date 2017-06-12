89770868fe3353cc8ea1de5394293dd616258b84
BiCheng <oscar71156@gmail.com>
2017-06-12 23:15:13 +0800

    unfished_foodlike

--- app/Http/Controllers/showshopcontroller.php
+++ app/Http/Controllers/showshopcontroller.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\shop;
 use App\Meal;
+use Auth;
 
 class showshopcontroller extends Controller
 {
@@ -23,8 +24,28 @@ class showshopcontroller extends Controller
     }
     public function showfood($shopid)
     {
+        if(Auth::check())
+        {
+            $userid=Auth::id();
+        }
+        DB::connection()->enableQueryLog();
         $shopmeals=DB::table('meals')->where('seller',$shopid)->get();
-        return view('foodinformation',['meals'=>$shopmeals]);
+        $likefood=DB::table('meals as meals1')->where('seller',$shopid)->Join('userlikefood as test','meals1.id','=','test.food');//->get()->leftjoin('userlikefoods'
+        //dd($likefood);
+        $likefood=DB::table('meals as meals1')->where('seller',$shopid)->LeftJoin('userlikefood','meals1.id','=','userlikefood.food')->select(DB::raw('count(userlikefood.food) as usernum, meals1.id as foodid, meals1.name as foodname, meals1.price as foodprice'))->groupBy('foodid')->get();
+        //select('meals.name as foodname','userlikefood.food as foodid','meals.price as food price",'userlikefood.food as foodid')->
+       // dd("$shopmeals");
+      //  dd($userlike);
+      /*foreach($shopmeals as $shopmeal)
+      {
+         
+
+      }*/
+        $queries=DB::getQueryLog();
+        //dd($queries);
+        dd($likefood);
+      //  die("<br>test");
+        return view('foodinformation',['meals'=>$shopmeals,'likefoodusernum'=>$likefood]);
     }
 }
 
--- resources/views/foodinformation.blade.php
+++ resources/views/foodinformation.blade.php
@@ -6,8 +6,20 @@
         <tbody id="shopinfor">
             <col width="25%"><col width="50%"><col widht="25%">
             <tr><th>like</th><th>名稱</th><th>價格</th></tr>
-            @foreach($meals as $meal)
-            <tr><td><button class="button"><span class="glyphicon glyphicon-heart" aria-hidden="true"></td> <td> {{ $meal->name}}</td><td> {{ $meal->price}}</td></tr>
+            @if($likefoodusernum->count()==0)
+            abc
+            @endif
+            @foreach ($likefoodusernum as $food)
+            <tr><td>
+                   
+                    <form>
+        
+                    <button type="submit" class="redbutton" style="color:red"><span class="glyphicon glyphicon-heart" aria-hidden="true"></button>
+    
+                    </form>
+                    
+            </td> 
+            <tr><td>$food->food</td><td>$food->usernum</td></tr>
             @endforeach
         </tbody>
     </table>
